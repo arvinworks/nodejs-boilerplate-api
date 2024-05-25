1d8b237f6f105cfbe94140a1c5a5a1fcfd87d2d6
VincentRice <Vincent.villacampo21@gmail.com>
2024-05-25 21:14:58 +0800

    More Updates

--- _helpers/db.js
+++ _helpers/db.js
@@ -7,28 +7,31 @@ module.exports = db = {};
 initialize();
 
 async function initialize() {
-  // create db if it doesn't already exist
-  const { host, port, user, password, database } = config.database;
-  const connection = await mysql.createConnection({ host, port, user, password });
-  await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
+    // create db if it doesn't already exist
+    const { host, port, user, password, database } = config.database;
+    const connection = await mysql.createConnection({ host, port, user, password });
+    await connection.query(`CREATE DATABASE IF NOT EXISTS \`${database}\`;`);
 
-  // connect to db
-  const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
+    // connect to db
+    const sequelize = new Sequelize(database, user, password, { dialect: 'mysql' });
 
-  // init models and add them to the exported db object
-  db.Account = require('../accounts/account.model')(sequelize);
-  db.Player = require('../players/player.model')(sequelize);
-  db.Team = require('../teams/team.model')(sequelize);
-  db.RefreshToken = require('../accounts/refresh-token.model')(sequelize);
-  db.Tournament = require('../tournament/tournament.model')(sequelize);
-  db.Schedule = require('../schedule/schedule.model')(sequelize);
+    // init models and add them to the exported db object
+    db.Account = require('../accounts/account.model')(sequelize);
+    db.Player = require('../players/player.model')(sequelize);
+    db.Team = require('../teams/team.model')(sequelize);
+    db.RefreshToken = require('../accounts/refresh-token.model')(sequelize);
+    db.Tournament = require('../tournaments/tournament.model')(sequelize); // Add the Tournament model
 
-  // define relationships
-  db.Team.hasMany(db.Player, { foreignKey: 'teamId' });
-  db.Player.belongsTo(db.Team, { foreignKey: 'teamId' });
-  db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
-  db.RefreshToken.belongsTo(db.Account);
+    // Define model relationships
+    db.Team.hasMany(db.Player, { foreignKey: 'teamId' });
+    db.Player.belongsTo(db.Team, { foreignKey: 'teamId' });
+    db.Account.hasMany(db.RefreshToken, { onDelete: 'CASCADE' });
+    db.RefreshToken.belongsTo(db.Account);
 
-  // sync all models with database
-  await sequelize.sync();
+    // Define relationships for tournaments
+    // Assuming each tournament has multiple teams and matches (if applicable)
+    db.Tournament.belongsToMany(db.Team, { through: 'TournamentTeams', foreignKey: 'tournamentId' });
+    db.Team.belongsToMany(db.Tournament, { through: 'TournamentTeams', foreignKey: 'teamId' });
+    // sync all models with database
+    await sequelize.sync();
 }
